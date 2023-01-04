3da3737a9c540390b920dbb78346d30253144216
JIWOOK KIM <38876771+tom919654@users.noreply.github.com>
2023-01-04 16:17:32 +0900

    Update __init__.py

--- im2scene/data/__init__.py
+++ im2scene/data/__init__.py
@@ -1,9 +1,8 @@
 
 from im2scene.data.datasets import (
-    LSUNClass, ImagesDataset,ImagesDataset_afhq)
+    ImagesDataset,ImagesDataset_afhq)
 
 __all__ = [
-    LSUNClass,
     ImagesDataset,
     ImagesDataset_afhq
 ]
